a06147557abc8e7e1432bb14d8601832189123c4
kaylamacaione <kmacaione@labelinsight.com>
2021-04-08 17:06:47 -0500

    final code

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -4,38 +4,41 @@ var tableData = data;
 //Define variables
 var tbody = d3.select("tbody");
 var button = d3.select("#filter-btn");
-var form = d3.select("#form");
 
-// YOUR CODE HERE!
 // Append table to web page
 data.forEach(function(tableData) {
-    console.log(tableData);
+    //console.log(tableData);
     var row = tbody.append("tr");
     Object.entries(tableData).forEach(function([key, value]) {
-      console.log(key, value);
+      //console.log(key, value);
       /// Add new rows for each UFO sighting
       var cell = row.append("td");
       cell.text(value);
     });
-  });
+});
 
 
 // Attach an event to detect changes to the input field.
-button.on("click", handleClick);
-form.on("submit",handleClick);
-
-
-  //Use a date form in your HTML document and write JavaScript code that will listen for events and search through the date/time column to find rows that match user input.
-  function handleClick(tableData) {
+button.on("click", function(handleClick){
     d3.event.preventDefault();
-    var inputElement = d3.select("#datetime");
-    var inputValue = inputElement.property("value");
-    console.log(inputValue);
-    var filteredTable = tableData.filter(d => {
-        return d.datetime === inputElement.property("value")
-      })
-    //var filteredTable = tableData.filter(d => d.datetime === inputElement.property("value"));
-    console.log(filteredTable);
-};
+    tbody.html("");
+//Search through the date/time column to find rows that match user input.
+var inputElement = d3.select("#datetime");
+var inputValue = inputElement.property("value");
+var filteredTable = tableData.filter(d => d.datetime === inputValue);
+
+//Append filtered table
+filteredTable.forEach(function(filteredDate){
+    var row = tbody.append("tr");
+    Object.entries(filteredDate).forEach(function([key,value]){
+        var cell=tbody.append("td");
+        cell.text(value);
+
+    });
+});
+
+console.log(filteredTable)
+});
+
 
 
